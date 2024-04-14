43ba6271f38550cf40584f984882f2b1011cd576
taku10101 <taku1011ht@gmail.com>
2024-04-15 01:27:27 +0900

    user repo add

--- interfaces/repository.go
+++ interfaces/repository.go
@@ -2,5 +2,11 @@ package interfaces
 
 import "github.com/taku10101/gin-clean/entities"
 type UserRepository interface {
-    FindByID(id int) (*entities.User, error)
+    Add(user *entities.User) error //*はポインタ型
+	FindAll() ([]*entities.User, error)
+ 	FindByID(id int) (*entities.User, error)
+	Update(user *entities.User) error
+	Delete(id int) error
+
+	
 }
